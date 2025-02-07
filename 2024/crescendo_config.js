192a9b2cfd16f482b23ca4e0d92c7e3b2c7a88a5
AaronH1272 <127373383+AaronH1272@users.noreply.github.com>
2025-02-06 20:08:33 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024mitr2",
+      "defaultValue": "2024miann",
       "required": "true"
     },
     { "name": "Match Level",
